9d4a2da7aecd09f929664826dd7924e9fd8d9a1c
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-06-18 18:04:35 +0200

    file upload working

--- app/server.py
+++ app/server.py
@@ -1,18 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
 from flask_cors import CORS
 
 import logging
 import argparse
 import os, sys
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './.upload/'
+ALLOWED_EXTENSIONS = {'pdf', 'txt'}
 
 app = Flask(__name__)
 CORS(app)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app.add_url_rule(
+    "/uploads/<name>", endpoint="download_file", build_only=True
+)
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
 
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('file_upload_info.html', download_url=url_for('download_file', name=filename), filename=filename)
+    
+    return render_template('file_upload.html')
+
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
 
 if __name__ == '__main__':
 
@@ -33,6 +71,9 @@ if __name__ == '__main__':
         if not os.path.exists(os.path.abspath(os.path.dirname(args.LOGFILE))):
                 os.makedirs(os.path.abspath(os.path.dirname(args.LOGFILE)))
 
+        if not os.path.exists(os.path.abspath(os.path.dirname(UPLOAD_FOLDER))):
+                os.makedirs(os.path.abspath(os.path.dirname(UPLOAD_FOLDER)))
+        
         # Setup Logging
         logging.basicConfig(filename=args.LOGFILE, level=logging.INFO if PRODUCTION else logging.DEBUG,
                         format='%(asctime)s %(levelname)-8s %(message)s')
