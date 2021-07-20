a5b3a3dfbd3fe922ae8de6f2648a07fc17e97db9
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-07-20 17:53:48 +0200

    attach to local service

--- app/PDF-APP-FRONTEND/src/app/wiki/wiki.service.ts
+++ app/PDF-APP-FRONTEND/src/app/wiki/wiki.service.ts
@@ -7,19 +7,20 @@ import { TextSpec } from '../commons/models.spec';
   providedIn: 'root'
 })
 export class WikiService {
-  private BE = "http://localhost/wiki/";
+  private BE = "http://localhost:5001/";
   constructor(private http: HttpClient) { }
 
   random(n: number = 1): Promise<Text | null> {
     return new Promise<Text | null>((resolve, reject) => {
-
-      resolve(new TextSpec())
-      return;
-      this.http.get<Text>(`${this.BE}/random`).toPromise<Text>().then((data: Text) => {
+      if(this.BE=="MOCK") {
+        resolve(new TextSpec())
+      } else {
+        this.http.get<Text>(`${this.BE}/random`).toPromise<Text>().then((data: Text) => {
           resolve(data)
-      }).catch((err) => {
-        reject(err);
-      });
+        }).catch((err) => {
+          reject(err);
+        });
+      }
     });
   }
 }
