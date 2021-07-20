2845f306b391df6e6bbb00910d8435dc1338ea8e
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-07-20 13:22:44 +0200

    display components of text

--- app/PDF-APP-FRONTEND/src/app/commons/text/text.component.ts
+++ app/PDF-APP-FRONTEND/src/app/commons/text/text.component.ts
@@ -7,10 +7,25 @@ import { Text } from '../models';
 })
 export class TextComponent implements OnInit {
   @Input() text: Text;
+  TITLE: string | null;
+  SHORT: string | null;
+  DESCRIPTION: string | null;
   constructor() { }
 
   ngOnInit() {
     if(!this.text) 
       throw new Error("Attribute 'text' is required");
+    
+    this.TITLE = this.getField('title');
+    this.SHORT = this.getField('short');
+    this.DESCRIPTION = this.getField('description');
+  }
+
+  private hasKey(key: string): boolean {
+    return Object.keys(this.text.text).includes(key)
+  }
+
+  getField(key: string): string | null {
+    return this.hasKey(key) ? this.text.text[key] : null
   }
 }
