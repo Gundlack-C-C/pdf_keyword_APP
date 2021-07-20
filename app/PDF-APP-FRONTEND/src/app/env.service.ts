4717e1df8329486575828009ed4d08c08f04ac24
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-07-20 18:05:32 +0200

    add environment service

---
+++ app/PDF-APP-FRONTEND/src/app/env.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class EnvService {
+
+constructor() { }
+
+  get SERVICE_WIKI(): string {
+    return environment.services.wiki
+  }
+
+  get SERVICE_PDF(): string {
+    return environment.services.pdf
+  }
+
+}
