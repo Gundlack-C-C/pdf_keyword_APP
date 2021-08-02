9cfee7d41173c10a2080ae7448a24859d7eea6d7
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-02 16:29:07 +0200

    requesting keywords from sklearn service working

--- app/PDF-APP-FRONTEND/src/app/sklearn/sklearn.component.ts
+++ app/PDF-APP-FRONTEND/src/app/sklearn/sklearn.component.ts
@@ -1,18 +1,25 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Text } from '../commons/models';
-
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { CorpusAnalytics, Text} from '../commons/models';
+import { SklearnService } from './sklearn.service';
 @Component({
   selector: 'app-sklearn',
   templateUrl: './sklearn.component.html',
   styleUrls: ['./sklearn.component.css']
 })
-export class SklearnComponent implements OnInit {
+export class SklearnComponent implements OnChanges {
   @Input() mode: string
   @Input() parameter: {[key: string]: string}
   @Input() text: Text
-  constructor() { }
+  res: CorpusAnalytics | null = null
+  constructor(private nlp: SklearnService) { }
 
-  ngOnInit() {
+  ngOnChanges(changes: SimpleChanges) {
+    if(changes['mode'] || changes['text'] || changes['parameter']) {
+      this.res = null;
+      let corpus = [this.text.Title, this.text.Short, this.text.Description].join('. ');
+      this.nlp.getKeywords(this.mode, corpus, this.parameter).then((res: CorpusAnalytics) => {
+        this.res = res;
+      })
+    }
   }
-
 }
