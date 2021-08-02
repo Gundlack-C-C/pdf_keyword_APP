3b22011015db07e495991d43948e9a9b21954afb
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-02 16:28:18 +0200

    remove initial set - just for debugging

--- app/PDF-APP-FRONTEND/src/app/views/text-analyse-view/text-analyse-view.component.ts
+++ app/PDF-APP-FRONTEND/src/app/views/text-analyse-view/text-analyse-view.component.ts
@@ -13,8 +13,8 @@ import { TextInputViewComponent } from '../text-input-view/text-input-view.compo
 })
 export class TextAnalyseViewComponent extends TextInputViewComponent implements OnInit {
   // Required form Algorithm execution
-  text: Text | null = new TextSpec()
-  parameter: {[key: string]: any} = {}
+  text: Text | null = null;
+  parameter: {[key: string]: any} = {};
 
   // Text Input
   options_input: {[key: string]: string[]} = TextInputSelectComponent.options;
