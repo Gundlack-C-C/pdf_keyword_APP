eca2694e659b922314065594b8a3f288dfa40546
Felix Gundlack <gundlackfelixdev@gmail.com>
2021-08-02 16:27:56 +0200

    POST request keywords Sklearn with json data

--- app/PDF-APP-FRONTEND/src/app/sklearn/sklearn.service.ts
+++ app/PDF-APP-FRONTEND/src/app/sklearn/sklearn.service.ts
@@ -1,6 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CorpusAnalytics, KeywordAnalytics, TextAnalytics } from '../commons/models';
 import { EnvService } from '../env.service';
+import { FormTfidfComponent } from './form-tfidf/form-tfidf.component';
 
 @Injectable({
   providedIn: 'root'
@@ -8,18 +10,28 @@ import { EnvService } from '../env.service';
 export class SklearnService {
   private BE: string;
   constructor(private env: EnvService, private http: HttpClient) { 
-    this.BE = this.env.SERVICE_PDF;
+    this.BE = this.env.SERVICE_SKLEARN;
   }
 
   getKeywords(mode: string, text: string,  param: {[key: string]: any}): Promise<any | null> {
     return new Promise<any>((resolve, reject) => {
-      const formData = new FormData()
-      formData.append('text', text)
-      formData.append('param', JSON.stringify(param))
-      this.http.post<any>(`${this.BE}/api/keywords/${mode}`, formData).toPromise<Text>().then((data: any) => {
-        resolve(data)
-      }).catch((err) => {
-        console.error(err)
+      let data = {
+        'text': text,
+        'param': param
+      }
+      mode = mode.toLowerCase().includes('tfidf') ? "tfidf" : "count"; 
+      this.http.post<any>(`${this.BE}/api/keywords/${mode}`,  JSON.stringify(data)).toPromise<CorpusAnalytics>().then((response: any) => {
+        let data_result = new CorpusAnalytics()
+        data_result.res = response['data'].map((item: any[]) => {
+          let sentence = item[0]
+          let keywords = item[1].map((key_item: any[]) => {
+              return new KeywordAnalytics(key_item[0], key_item[1], key_item[2]);
+          })
+          return new TextAnalytics(sentence, keywords)
+        })
+        resolve(data_result)
+      }).catch((err: HttpErrorResponse) => {
+        console.error(err.message)
         reject(err);
       });
     });
